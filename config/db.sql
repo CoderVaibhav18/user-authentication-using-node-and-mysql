cfc7b110a12f809ee2f41914115f29b6a4173462
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-16 17:53:01 +0530

    Add db info

---
+++ config/db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE user_auth;
+USE user_auth;
+
+TRUNCATE TABLE users;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+DELETE FROM users;
+SELECT * FROM users;
