fc4c1f8d782cc47c278180f942c10ec409454957
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-16 17:38:13 +0530

    `Added cors middleware and updated express.json and express.urlencoded configurations`

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cookiesParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookiesParser());
 
 app.use("/api/auth", authRoutes);
