4ef1b31ba182eec8d15c95996a08e9b163d34b3c
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-16 16:47:07 +0530

    login done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
-const db = require('../config/db');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const db = require("../config/db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const signup = (req, res) => {
   const { name, email, password } = req.body;
@@ -24,6 +24,7 @@ const signup = (req, res) => {
         "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
         [name, email, hashedPassword],
         (err, result) => {
+          console.log(result);
           if (err) throw err;
           res.status(201).json({ message: "User registered successfully" });
         }
@@ -31,7 +32,36 @@ const signup = (req, res) => {
     }
   );
 };
-const login = () => {};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  db.query(
+    "SELECT * FROM users WHERE email= ?",
+    [email],
+    async (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, result[0].password);
+
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign({ id: result[0].id }, process.env.JWT_SECRET);
+      res.status(200).json({ msg: "login successfully",
+        token: token
+       });
+    }
+  );
+};
 
 module.exports = {
   signup,
